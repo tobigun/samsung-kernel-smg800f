cdafdf59749a3dc39222429f61dd2f973c595cf3
tobigun <hennymcc@yahoo.de>
2014-12-09 22:19:34 +0100

    initial commit of kernel G800FXXU1ANG1 (SM-G800F_KK_Opensource_Update1)

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Jul 8 21:51:14 KST 2014"
+#define LINUX_COMPILE_BY "omsgsm"
+#define LINUX_COMPILE_HOST "sy918-desktop"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
